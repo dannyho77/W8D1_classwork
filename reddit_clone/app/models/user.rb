08f384e1eed97ca9b6c26038dbc64de149a1f8e3
danny <dannyho77@gmail.com>
2021-02-15 10:35:50 -0500

    started User model -- auth methods

---
+++ reddit_clone/app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ApplicationRecord
+    validates :username, :session_token, :password_digest, presence: true
+    validates :username, :session_token, uniqueness: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    attr_reader :password
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom.base64
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+end
